6271608a7bcd103e10ff6000779a9eb2b54cfa7e
arnab8788 <70602952+arnab8788@users.noreply.github.com>
2021-08-21 22:49:53 +0530

    Add files via upload

---
+++ 01_Print_Programs/01_PrintProgram.py
@@ -0,0 +1,54 @@
+# Basic Print Statement
+
+# This will print a simple string
+print('Hello! World.');
+
+# This will print a number
+print(1);
+
+# This will concatenate a string with + symbol and print
+print('Hello' + '! ' + 'World.');
+
+# This will print the content of a variable
+myvariable='This is a variable!';
+print(myvariable);
+
+# This will print the concatenation of two variables
+myvariable_1 = 'This is variable 1.';
+myvariable_2 = 'This is variable 2.';
+print(myvariable_1 + '---' + myvariable_2);
+
+#Arithmetic operation in print statement
+mynumber1=10;
+mynumber2=20;
+mynumber3=30;
+mynumber4=40;
+print((mynumber1 + mynumber2) / mynumber3 * mynumber4);
+
+#Using single quotes in the string using double quotes
+print("I didn't do it");
+
+#Using single quotes in the string using escape character
+print('I didn\'t do it');
+     
+#Using double quotes in the string using single quotes
+print('My name is "Arnab" Desarkar!');
+
+#Using double quotes in the string using escape character
+print("My name is \"Arnab\" Desarkar!");
+
+
+##############################################################################
+#Below is the Ouput provided for each of the print statements#
+##############################################################################
+#Hello! World.
+#1
+#Hello! World.
+#This is a variable!
+#This is variable 1.---This is variable 2.
+#40
+#I didn't do it
+#I didn't do it
+#My name is "Arnab" Desarkar!
+#My name is "Arnab" Desarkar!
+##############################################################################
